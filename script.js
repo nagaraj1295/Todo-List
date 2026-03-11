68eedc45d9a6389d0c9ff947e3591cbf4bb80fcf
NAGARAJ C <rjnaga123@gmail.com>
2026-03-11 14:30:08 +0530

    Delete button inserted

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 let outtxt = document.getElementById("tasksl");
 function add(){
     let intxt = document.getElementById("intext").value;
-    outtxt.innerHTML +="<li>"+intxt+"</li>";  
+    outtxt.innerHTML +="<li>"+intxt+"<button onclick=dlt(this) id='dlt'>❌</button>"+"</li>";  
+}
+
+function dlt(btn){
+    btn.parentElement.remove();
 }
